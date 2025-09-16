1324bda878e19d337dda8c62c9b2f7e051f3129e
Silverkts <david.mendiganom@gmail.com>
2025-09-16 16:23:13 -0500

    Update formu.php

--- formu.php
+++ formu.php
@@ -3,11 +3,11 @@ $Nombre= $_POST  ['Nombre'];
 $Curso=$_POST ['Curso en formacion'];
 $Numero= $_POST  ['Ingrese un numero aleatoreo'];
 
-$val = $Numero * $Numero
+$val = $Numero * $Numero * $Numero
 
 echo "Su nombre es: $Nombre <br>";
 echo "ESu curso es: $Curso <br>";
 if ($val >= 100)  {
-  echo "su numero es: . $val . que afortunado eh";
+  echo "su numero es:". $val ."que afortunado eh";
 }else  {
   echo "su numero es: $val";  }
