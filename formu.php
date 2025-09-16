61fac169e0a3a40122809ae39134c85e2e2e775a
Silverkts <david.mendiganom@gmail.com>
2025-09-16 16:11:05 -0500

    Update formu.php

--- formu.php
+++ formu.php
@@ -1,18 +1,13 @@
 <?php
-$Producto1= $_POST  ['Producto1'];
-$Valor1=$_POST ['Valor1'];
-$Producto2= $_POST  ['Producto2'];
-$Valor2=$_POST ['Valor2'];
-$Producto3= $_POST  ['Producto3'];
-$Valor3=$_POST ['Valor3'];
+$Nombre= $_POST  ['Nombre'];
+$Curso=$_POST ['Curso en formacion'];
+$Numero= $_POST  ['Ingrese un numero aleatoreo'];
 
-$suma = $Valor1 + $Valor2 + $Valor3;
-$descuento = $suma * 0.16;
-$totdescuento = $suma - $descuento;
+$val = $Numero * $Numero
 
-echo "El nombre del producto1 es: $Producto1 <br>";
-echo "El nombre del producto2 es: $Producto2 <br>";
-echo "El nombre del producto3 es: $Producto3 <br>";
-echo "La suma de los 3 precios es: $suma <br>";
-echo "Y si a ese valor se le añade un descuento del 0.16% da: $totdescuento <br>";
-?>
+echo "Su nombre es: $Nombre <br>";
+echo "ESu curso es: $Curso <br>";
+if ($val >= 100)  {
+  echo "su numero es: . $val . que afortunado eh";
+}else  {
+  echo "su numero es: $val";  }
